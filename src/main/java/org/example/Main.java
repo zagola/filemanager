66733a4d328d04660e34762766537f76e0d8929a
zagola <zagola@github.com>
2024-02-23 15:41:03 +0100

    Change in folder path for Main.class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         FileManager fileManager = new FileManager();
-        fileManager.sortFilesInDirectory("/home/ola/Documents/books/kindle/onkindle");
+        fileManager.sortFilesInDirectory("/home/ola/Documents/books/kindle/toread");
 
     }
 }
