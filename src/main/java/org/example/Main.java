901f23ff4097dce1ca3bca52c052c357aa4928ec
zagola <zagola@github.com>
2024-02-23 15:34:04 +0100

    Main.class

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        FileManager fileManager = new FileManager();
+        fileManager.sortFilesInDirectory("/home/ola/Documents/books/kindle/onkindle");
+
+    }
+}
